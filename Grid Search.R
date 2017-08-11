3921d292894fc3f85671feab20d699305d12e175
yifan27 <30607277+yifan27@users.noreply.github.com>
2017-08-10 20:38:30 -0500

    Add files via upload

---
+++ Grid Search.R
@@ -0,0 +1,60 @@
+# Grid Search
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+
+# Applying Grid Search to find the best parameters
+# install.packages('caret')
+library(caret)
+classifier = train(form = Purchased ~ ., data = training_set, method = 'svmRadial')
+classifier
+classifier$bestTune
+
+# link https://topepo.github.io/caret/available-models.html
+
+
+
+#########TEST#########not yet
+
+# Fitting Kernel SVM to the Training set
+# install.packages('e1071')
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial')
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+cm
+
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial',
+                 sigma=1.340817)
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+cm
